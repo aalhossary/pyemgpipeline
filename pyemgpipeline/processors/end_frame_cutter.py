77fb5562948efcc55683e21dc9ea09de4a7c24ef
tlwu <tlwu2008@gmail.com>
2021-12-06 13:25:32 +0800

    Fix bug for zero n_end_frames

--- pyemgpipeline/processors/end_frame_cutter.py
+++ pyemgpipeline/processors/end_frame_cutter.py
@@ -2,15 +2,17 @@ from . base import BaseProcessor
 
 
 class EndFrameCutter(BaseProcessor):
-    """End frame cutter
+    """End frame cutter for EMG signals
 
     Parameters
     ----------
     n_end_frames : int, default=30
         Number of frames to be cut off in both ends of the signal.
+        n_end_frames >= 0.
     """
 
     def __init__(self, n_end_frames=30):
+        assert n_end_frames >= 0, 'n_end_frames must be non-negative'
         self.n_end_frames = n_end_frames
 
     def apply(self, x, **kwargs):
@@ -34,7 +36,10 @@ class EndFrameCutter(BaseProcessor):
         super().assert_input(x)
         assert x.shape[0] > 2 * self.n_end_frames, 'first dimension of x must have length > 2 * n_end_frames'
 
-        x_processed = x[self.n_end_frames:(-self.n_end_frames), ]
+        if self.n_end_frames > 0:
+            x_processed = x[self.n_end_frames:(-self.n_end_frames), ]
+        else:
+            x_processed = x
         return x_processed
 
     def get_parameter_str(self):
